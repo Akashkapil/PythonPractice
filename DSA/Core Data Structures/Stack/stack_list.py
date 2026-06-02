267db1bea4de355e9d2580b40e8fd2c4388a46b8
Akash <akashkapil06@gmail.com>
2026-06-02 21:26:44 +0530

    stack using list

---
+++ DSA/Core Data Structures/Stack/stack_list.py
@@ -0,0 +1,12 @@
+# Python does not have a built-in stack type, so it can be implimented using various methods.
+
+# Stack using List
+
+st = []
+st.append('a')
+st.append('b')
+st.append('c')
+
+print(st)
+print(st.pop())
+print(st.pop())
